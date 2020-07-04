91b87fff665eba7f9bc56ec3f72beeefb1945b8e
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-07-03 20:05:32 -0500

    init

---
+++ several.js
@@ -0,0 +1,10 @@
+const capitalize = (string) => string.split('')[0].toUpperCase() + string.slice(1);
+
+const stringReverse = (string) => string.split('').reverse().join('');
+
+const calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  divide: (a, b) => a / b,
+  multiply: (a, b) => a * b,
+};
